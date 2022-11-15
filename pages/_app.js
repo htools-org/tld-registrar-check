d3d737ef6b2439d3de94daf9b222957a52754e26
Rithvik Vibhu <rithvikvibhu@gmail.com>
2022-11-15 16:03:08 +0530

    add umami

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import Script from "next/script"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
+        process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+          <Script
+            src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="lazyOnload"
+          />
+        )}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
